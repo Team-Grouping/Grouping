6faef8ac0cada9225a865d2e8253941900ff4f26
smreosms13 <smreosms13@gmail.com>
2024-09-08 17:16:30 +0900

    feat: main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,48 @@
-import styles from "./page.module.css";
+"use client";
+import { MouseEvent, useEffect } from "react";
+import styles from "./page.module.scss";
+import { useWeb3Auth } from "@web3auth/modal-react-hooks";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function Home() {
+  const { isConnected, connect, web3Auth } = useWeb3Auth();
+  const router = useRouter();
+
+  const handleLogin = async (event: MouseEvent<HTMLButtonElement>) => {
+    if (web3Auth) {
+      if (isConnected) {
+        router.push("/products");
+      } else {
+        await connect();
+        if (isConnected) {
+          router.push("/products");
+        }
+      }
+    }
+  };
+
   return (
     <main className={styles.main}>
-      login!! push the home button in bottobar
+      <div className={styles.center}>
+        <h1 className={styles.title}>Grouping</h1>
+        <div className={styles.description}>
+          <h2>A decentralized Web 3.0 social commerce platform</h2>
+        </div>
+        <Image
+          className={styles.logo}
+          src="/images/GroupingLogo.png"
+          alt="Grouping"
+          width={200}
+          height={200}
+        />
+        <div className={styles.description}>
+          <p>enables safe and transparent group purchasing</p>
+        </div>
+      </div>
+      <button className={styles.button} onClick={handleLogin}>
+        Try Grouping!
+      </button>
     </main>
   );
 }
