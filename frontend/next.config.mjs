703ec2d3b7c7c31a6adadfde6dde135f29c52aa8
smreosms13 <smreosms13@gmail.com>
2024-09-08 21:46:37 +0900

    hotfix : config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,7 +3,7 @@ import path from "path";
 
 const nextConfig = {
   webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve("./src");
+    config.resolve.alias["@"] = path.resolve("src");
     return config;
   },
 };
